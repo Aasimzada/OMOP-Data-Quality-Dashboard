679ff34ac1e09ccabacba90e9d8951c404635e2a
Aasimzada <163043181+Aasimzada@users.noreply.github.com>
2024-03-13 15:10:58 -0400

    Add files via upload

---
+++ Data_Quality_Scripts/python/field_cdm_field.py
@@ -0,0 +1,22 @@
+# Check name - measurePersonCompleteness
+
+#A yes or no value indicating if field is present in the  table as expected based on the specification.
+
+import pandas as pd
+
+# Read data from CSV files or 
+df_your_table =  pd.read_csv('path to your file/your_table.csv')
+
+# Check if the field exists
+num_violated_rows = 1 if 'gender_concept_id' not in df_your_table.columns else 0
+
+# Calculate denominator
+num_denominator_rows = 1
+
+# Calculate percentage of violated rows
+pct_violated_rows = num_violated_rows / num_denominator_rows
+
+# Output results
+print("Number of violated rows:", num_violated_rows)
+print("Percentage of violated rows:", pct_violated_rows)
+print("Total number of rows in the PERSON table:", num_denominator_rows)

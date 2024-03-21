7758673dabc72cdf48353ace645e6aa06d3978d3
Aasimzada <a.sahibzada@ufl.edu>
2024-03-21 14:25:47 -0400

    commit changes

---
+++ Data_Quality_Scripts/sql/field_measure_value_completeness.sql
@@ -0,0 +1,43 @@
+
+/*********
+MEASURE_VALUE_COMPLETENESS
+Computing number of null values and the proportion to total records per field
+
+Parameters used in this template:
+cdmDatabaseSchema = @cdmDatabaseSchema
+cdmTableName = @cdmTableName
+cdmFieldName = @cdmFieldName
+
+**********/
+
+
+SELECT 
+	num_violated_rows, 
+	CASE 
+		WHEN denominator.num_rows = 0 THEN 0 
+		ELSE 1.0*num_violated_rows/denominator.num_rows 
+	END AS pct_violated_rows, 
+  denominator.num_rows AS num_denominator_rows
+FROM
+(
+	SELECT 
+		COUNT_BIG(violated_rows.violating_field) AS num_violated_rows
+	FROM
+	(
+		/*violatedRowsBegin*/
+		SELECT 
+			'@cdmTableName.@cdmFieldName' AS violating_field, 
+			cdmTable.* 
+		FROM @cdmDatabaseSchema.@cdmTableName
+			
+		WHERE cdmTable.SPONSOR_CONCEPT_ID IS NULL
+		/*violatedRowsEnd*/
+	) violated_rows
+) violated_row_count,
+( 
+	SELECT 
+		COUNT_BIG(*) AS num_rows
+	FROM @cdmDatabaseSchema.@cdmTableName
+	  
+) denominator
+;

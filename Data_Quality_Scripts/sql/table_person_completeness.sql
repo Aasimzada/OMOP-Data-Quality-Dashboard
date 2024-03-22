5867a74258577d7c36a5cab7032a3335f2761db4
Aasimzada <a.sahibzada@ufl.edu>
2024-03-22 13:10:36 -0400

    code changes

--- Data_Quality_Scripts/sql/table_person_completeness.sql
+++ Data_Quality_Scripts/sql/table_person_completeness.sql
@@ -2,7 +2,7 @@
 /*********
 Table Level:  
 MEASURE_PERSON_COMPLETENESS
-Determine what #/% of persons have at least one record in the cdmTable
+Determine what # / % of persons have at least one record in the cdmTable
 
 Parameters used in this template:
 schema = @schema
